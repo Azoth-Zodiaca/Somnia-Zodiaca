0e454d66695fbe95ed484eece39236d5282ba516
Hutch9910 <alberto.camo.ac@gmail.com>
2026-07-29 13:10:55 +0200

    first commit

---
+++ src/main/java/com/azoth/somniazodiaca/SomniazodiacaApplication.java
@@ -0,0 +1,13 @@
+package com.azoth.somniazodiaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomniazodiacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomniazodiacaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azoth/somniazodiaca/SomniazodiacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azoth.somniazodiaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomniazodiacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
