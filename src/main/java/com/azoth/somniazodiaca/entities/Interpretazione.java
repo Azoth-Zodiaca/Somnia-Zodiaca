7554313f3b7c748ca14a47d7c470887fce0cc747
MaurizioCarugo <51837168+MaurizioCarugo@users.noreply.github.com>
2026-07-30 17:00:53 +0200

    la vera rifinitura

--- src/main/java/com/azoth/somniazodiaca/entities/Interpretazione.java
+++ src/main/java/com/azoth/somniazodiaca/entities/Interpretazione.java
@@ -1,14 +1,11 @@
 package com.azoth.somniazodiaca.entities;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
