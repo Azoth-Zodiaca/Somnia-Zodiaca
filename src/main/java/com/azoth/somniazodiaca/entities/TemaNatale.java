bed23a36e116b6ea8cce8cad4f6cd31366fe38eb
Hutch9910 <alberto.camo.ac@gmail.com>
2026-07-30 12:41:34 +0200

    UtenteDto e TemaNataleDto

--- src/main/java/com/azoth/somniazodiaca/entities/TemaNatale.java
+++ src/main/java/com/azoth/somniazodiaca/entities/TemaNatale.java
@@ -11,7 +11,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -38,13 +37,13 @@ public class TemaNatale extends BaseEntity {
     @Column(name = "luogo_nascita", nullable = false)
     private String luogoNascita;
 
-    @Column(precision = 10, scale = 7)
+    @Column(name = "latitudine", precision = 10, scale = 7)
     private BigDecimal latitudine;
 
-    @Column(precision = 10, scale = 7)
+    @Column(name = "longitudine", precision = 10, scale = 7)
     private BigDecimal longitudine;
 
-    @Column(length = 100)
+    @Column(name = "timezone", length = 100)
     private String timezone;
 
     @Column(name = "data_creazione", nullable = false)
